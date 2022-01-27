7d585f76d25fbd638a17029c062c8bf68dcb9838
zhinobu38 <ake.38@hotmail.com>
2022-01-27 15:22:30 +0700

    study

---
+++ js38/3.js
@@ -0,0 +1,18 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+let circle = new Circle(2);
+
+for (let key in circle) {
+    if (typeof circle[key] !== 'function') {
+        console.log(key, circle[key]);
+    }
+}
+
+if ('radius' in circle) {
+    console.log('Radius is in the circle.');
+}
