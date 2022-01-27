f82eb8dffe98371f88a7da0592f41c1266d9bfac
zhinobu38 <ake.38@hotmail.com>
2022-01-27 11:18:26 +0700

    Update second.js

--- js38/second.js
+++ js38/second.js
@@ -1,3 +1,4 @@
+//Factory function
 function createCircle(radius) {
     return {
         radius,
@@ -11,3 +12,13 @@ const circle1 = createCircle(1);
 const circle2 = createCircle(2);
 circle1.surface();
 circle2.surface();
+
+//Constructor
+function Circle(radius) {
+    this.radius = radius;
+    this.surface = function(){
+        console.log('Surface of ' + this.radius + 'm radius circle = ' + (this.radius*3.14));
+    };
+}
+const another = new Circle(3);
+another.surface();
